9dbe7bcdeb55a644da25f5750dff2cbc7bc6bd9c
Talon Navarez <navareztalon@gmail.com>
2017-01-13 09:14:52 -1000

    cats and dogs

---
+++ fortune-cookie-generator/apps.js
@@ -0,0 +1,19 @@
+console.log("Generator");
+
+var generatefortunecookie = function(){
+	var fortunes = fortuneslist[Math.floor((Math.random() * 10) + 1)]
+	var node = document.createElement("li");
+	var fortunecookietext = getElementid()
+}
+
+var fortuneslist = [
+"Death by fire.",
+"Death by raid.",
+"Find $1,000,000,000.",
+"Gain immortality.",
+"Be given pie.",
+"Meet the live of your life.",
+"Lose a loved one.",
+"learn from your mistakes.",
+"Be the next Einstein.",
+"Invent the NEXT BIG THING."]
---
+++ quiz/array_and_objects.js
@@ -0,0 +1,18 @@
+var myArray = ["person", "car", 10, true]{
+	person : "Talon";
+	car : "honda";
+	number : 10;
+	boolean : true;
+}
+
+function Array(h){
+	if (myArray.boolean = "true"){
+		console.log(myArray.person + " is " + myArray.number + " years old, but he is allowed to drive the family " + myArray.car){
+		}
+	}else{
+		console.log("YOU SHALL NOT PASS!!"){
+		}
+	}
+} 
+
+Array(h);
